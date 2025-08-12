5e7f2b7a59a7ccb2213b141b2eef4f66eca1a0d2
Dracconiac <martin.cernak.se@gmail.com>
2025-08-12 22:24:40 +0200

    void ClimbLadder()
    created ClimbLadder() method in CharacterController.cs that starts and stops the climbing animations based on the movement input on the ladder.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,36 +8,47 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3.5f;
+    [SerializeField] float climbSpeed = 2f;
     [SerializeField] float jumpForce = 5f;
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator animator;
     Collider2D playerCollider;
     LayerMask layerMaskGround;
+    LayerMask layerMaskLadder;
     bool isRunning = false;
-
+    bool isClimbing = false;
     int JumpStateHash;
+    int ClimbStateHash;
     int jumpCount = 0;
+
+    float initGravity = 1f;
+    float ladderGravity = 0f;
+    float stopAnimation = 0f;
+    float restartAnimation = 1f;
 
 
     void Start()
     {
         layerMaskGround = LayerMask.GetMask("Ground");
+        layerMaskLadder = LayerMask.GetMask("Ladders");
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<Collider2D>();
-        JumpStateHash = Animator.StringToHash("Base Layer.jump");
+        SetAnimationHash();
     }
 
     void Update()
     {
         isRunning = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+        isClimbing = Math.Abs(moveInput.y) > Mathf.Epsilon;
         if (playerCollider.IsTouchingLayers(layerMaskGround))
         {
             jumpCount = 0;
         }
         StartRunning();
         FlipPlayer();
+        ClimbLadder();
     }
     void OnMove(InputValue value)
     {
@@ -60,7 +71,14 @@ public class CharacterController : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
         rb.velocity = playerVelocity;
-        animator.SetBool("isRunning", isRunning);
+        if (playerCollider.IsTouchingLayers(layerMaskGround))
+        {
+            animator.SetBool("isRunning", isRunning);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
     }
 
     void FlipPlayer()
@@ -68,6 +86,32 @@ public class CharacterController : MonoBehaviour
         if (isRunning)
         {
             transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if (playerCollider.IsTouchingLayers(layerMaskLadder))
+        {
+            rb.gravityScale = ladderGravity;
+            Vector2 climbVelocity = new Vector2(rb.velocity.x, moveInput.y * climbSpeed);
+            rb.velocity = climbVelocity;
+            animator.SetBool("isClimbing", true);
+            if (!isClimbing)
+            {
+                animator.speed = stopAnimation;
+            }
+            else
+            {
+                animator.speed = restartAnimation;
+            }
+        }
+        else
+        {
+            rb.gravityScale = initGravity;
+            animator.SetBool("isClimbing", false);
+            animator.speed = restartAnimation;
+            return;
         }
     }
 
@@ -85,5 +129,11 @@ public class CharacterController : MonoBehaviour
         {
             animator.SetBool("isMidair", true);
         }
+    }
+
+    void SetAnimationHash()
+    {
+        JumpStateHash = Animator.StringToHash("Base Layer.jump");
+        ClimbStateHash = Animator.StringToHash("Base Layer.Ladder");
     }
 }
